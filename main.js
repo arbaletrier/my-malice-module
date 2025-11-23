9dea6b8c1bc8e2da469f05f391bc2951fcd0572c
arbaletrier <63588946+arbaletrier@users.noreply.github.com>
2025-11-23 11:31:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-console.log("Malice Damage Splitter起動");
-
-
-/******************************************************
- * Malice Damage Splitter (Foundry Module)
- * Author: あなた
- * Foundry 起動時に常駐し、Malice ダメージだけ Aura Actor に送る
- ******************************************************/
-
 Hooks.once("init", () => {
   console.log("Malice module loaded!");
   Hooks.on("createChatMessage", (msg, opts, userId) => {
